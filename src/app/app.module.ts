5e45d9b82bd342637bcd1f658cf913d4d5d63463
Yash Wadkar <153938514+Yashwadkar2121@users.noreply.github.com>
2024-09-22 02:04:45 +0530

    Angular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdmindashComponent } from './admindash/admindash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdmindashComponent
   ],
   imports: [
     BrowserModule,
